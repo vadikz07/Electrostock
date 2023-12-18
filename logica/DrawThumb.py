fbf95ae71fd8364a40e71632f647d6161ac9f535
vadik <vadikz@gmail.com>
2023-12-18 19:02:41 +0100

    Mejorada salida de imagenes

--- logica/DrawThumb.py
+++ logica/DrawThumb.py
@@ -2,6 +2,9 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 
 class DrawThumb:
+    size_xxl = 190
+    size_bold = 45
+    size_normal = 35
     def __init__(self, num, name, model, imgpath) -> None:
         print('inicializando draw')
         self.num = num
@@ -12,13 +15,15 @@ class DrawThumb:
             self.create_img()
         
     def create_img(self):
-        width, height = 400, 200
+        width, height = 600, 300
         image = Image.new("RGB", (width, height), "white") #base
         drawing = ImageDraw.Draw(image)
-        font_bold = ImageFont.truetype('arialbd.ttf', size=20)
-        font = ImageFont.truetype('arial.ttf',size=14)
-        drawing.text((10, 10), f"Nombre: {self.name}", fill="black", font=font_bold)
-        drawing.text((10, 40), f"Modelo: {self.model}", fill="black", font=font)
+        font_xxl = ImageFont.truetype('arialbd.ttf', size=self.size_xxl)
+        font_bold = ImageFont.truetype('arialbd.ttf', size=self.size_bold)
+        font = ImageFont.truetype('arial.ttf',size=self.size_normal)
+        drawing.text((350,10), f"{self.num}", fill="black", font=font_xxl)
+        drawing.text((10, 10), f"{self.name}", fill="black", font=font_bold)
+        drawing.text((10, 80), f"{self.model}", fill="black", font=font)
         if self.imgpath != 'Sin imagen':
             img_component = Image.open(self.imgpath)
             image.paste(img_component, (10,70))
