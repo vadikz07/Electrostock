3ad819d0eaff4c2d08e1d1c3191edbecb29b4486
vadik <vadikz@gmail.com>
2023-12-28 06:07:52 +0100

    Funcionalidad cargar imagenes desde URL

--- interfaz/componentes/previewImg.py
+++ interfaz/componentes/previewImg.py
@@ -4,15 +4,25 @@ import ttkbootstrap as tb
 from ttkbootstrap.icons import Icon
 from logica.getDataLogic import *
 from PIL import Image, ImageTk
+import urllib.request
+import io
 
 
 class PhotoImageLabel(tb.Label):
     max_size = (300,300)
-    def __init__(self, parent, **kwargs):
-        image = Image.open(kwargs['image'])
-        image.thumbnail(self.max_size)
-        self._image = ImageTk.PhotoImage(image)
-        
+    def __init__(self, parent, local=False, **kwargs):
+        if local == False:
+            with urllib.request.urlopen(kwargs['image']) as u:
+                raw_data = u.read()
+            # image = Image.open(kwargs['image'])
+            image = Image.open(io.BytesIO(raw_data))
+            image.thumbnail(self.max_size)
+            self._image = ImageTk.PhotoImage(image)
+            
+        else:
+            image = Image.open(kwargs['image'])
+            image.thumbnail(self.max_size)
+            self._image = ImageTk.PhotoImage(image)
         kwargs['image'] = self._image
         super().__init__(parent, **kwargs)
         
@@ -26,23 +36,18 @@ class PreviewWindow:
         self.original_data = retrieve_item(uuid_target=self.uuid_to_modify)
         self.newWindow = tk.Toplevel(self.root)
         self.newWindow.title('Imagen')
-        self.newWindow.geometry('450x400')
         self.maincontainer = tb.Frame(master=self.newWindow)
         self.maincontainer.pack(**self.common_options_pack)
         self.labeltext = tb.LabelFrame(master=self.maincontainer, text=f'{self.original_data["nombre"]}: {self.original_data["modelo"]}')
         self.labeltext.pack(**self.common_options_pack)
 
 
-        
-        
-        # self.control_lblframe = tb.LabelFrame(master=self.maincontainer, text='Acciones')
-        # self.control_lblframe.pack(**self.common_options_pack)
-        
-        # self.mod_url_img_btn = tb.Button(master=self.control_lblframe, text='Modificar URL imagen')
-        # self.mod_url_img_btn.pack(**self.common_options_pack)
-
     def show_image(self):
-        self.photo_label = PhotoImageLabel(self.labeltext, image='data/imgs/Retro.png')
-        self.photo_label.pack()
-        self.url_lbl = tb.Label(master=self.labeltext, text=f'URL: {self.original_data["imagen"]}')
-        self.url_lbl.pack()
+        if self.original_data['imagen'] != '':
+            self.photo_label = PhotoImageLabel(self.labeltext, image=self.original_data['imagen'],local=False)
+            self.photo_label.pack()
+        else:
+            self.photo_label = PhotoImageLabel(self.labeltext, image="data/imgs/noimage.png",local=True)
+            self.photo_label.pack()
+        # self.url_lbl = tb.Label(master=self.labeltext, text=f'URL: {self.original_data["imagen"]}')
+        # self.url_lbl.pack()
